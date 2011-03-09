c5c4658306afb94000f888379d8e4a0db1cafec9
Jon Eyolfson <jon@eyolfson.com>
2011-03-09 10:33:18 -0500

    Finished a larger random sample.

--- random_sample.py
+++ random_sample.py
@@ -17,8 +17,9 @@ if args.config == "postgres.ini":
 elif args.config == "linux.ini":
     exclude_sha1 = '1da177e4c3f41524e886b7f1b8a0c1fc7321cac2'
 
-fix_commits = []
-non_fix_commits = []
+# fix_commits = []
+# non_fix_commits = []
+commits = []
 
 for commit in repository.iter_commits('master'):
     if exclude_sha1 != commit.hexsha:
@@ -27,19 +28,46 @@ for commit in repository.iter_commits('master'):
         else:
             entry = (commit.hexsha, False, commit.message)
 
+        # if not entry[1] and fix_re.search(commit.message):
+        #     fix_commits.append(entry)
+        # else:
+        #     non_fix_commits.append(entry)
+
         if not entry[1] and fix_re.search(commit.message):
-            fix_commits.append(entry)
+            commits.append((entry, True))
         else:
-            non_fix_commits.append(entry)
+            commits.append((entry, False))
+
+# output = open('random-sample.txt', 'wb')
+# output.write('=== Fix Commits ===\n')
+# for entry in random.sample(fix_commits, 100):
+#     output.write('Commit: %s\n' % entry[0])
+#     output.write('Message:\n%s' % entry[2])
+#     output.write('Comment:\n\n')
+# output.write('=== Non-Fix Commits ===\n')
+# for entry in random.sample(non_fix_commits, 50):
+#     output.write('Commit: %s\n' % entry[0])
+#     output.write('Merge: %s\n' % entry[1])
+#     output.write('Message:\n%s' % entry[2])
+#     output.write('Comment:\n\n')
+# output.close()
+
+fix_commits = []
+non_fix_commits = []
+for entry in random.sample(commits, 200):
+    if entry[1] == True:
+        fix_commits.append(entry[0])
+    else:
+        non_fix_commits.append(entry[0])
 
 output = open('random-sample.txt', 'wb')
-output.write('=== Fix Commits ===\n')
-for entry in random.sample(fix_commits, 100):
+output.write('=== Fix Commits (%d) ===\n' % len(fix_commits))
+for entry in fix_commits:
     output.write('Commit: %s\n' % entry[0])
     output.write('Message:\n%s' % entry[2])
     output.write('Comment:\n\n')
-output.write('=== Non-Fix Commits ===\n')
-for entry in random.sample(non_fix_commits, 50):
+output.write('=== Non-Fix Commits (%d) ===\n' % len(non_fix_commits))
+for entry in non_fix_commits:
     output.write('Commit: %s\n' % entry[0])
     output.write('Merge: %s\n' % entry[1])
     output.write('Message:\n%s' % entry[2])
